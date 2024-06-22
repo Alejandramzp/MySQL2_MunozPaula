35871ce0f7e7126c59af368965b10a6d3dc83d9f
Paula Muñoz <156429313+Alejandramzp@users.noreply.github.com>
2024-06-21 19:10:52 -0500

    feat: :sparkles: Consulta Multitabla

--- Dia_2/Ejercicio_1.sql
+++ Dia_2/Ejercicio_1.sql
@@ -194,9 +194,16 @@ SELECT nombre,presupuesto(presupuesto,gastos) AS valor_presupuesto
 FROM departamento;
 
 -- 12.Lista el nombre de los departamentos y el valor del presupuesto actual ordenado de forma ascendente.
-SELECT nombre, presupuesto(presupuesto,gastos) AS valor_presupuesto -- FUNCION presupuesto
-FROM departamento
-ORDER BY valor_presupuesto ASC;
+DELIMITER //
+CREATE PROCEDURE nombreD_presupuestoActual()
+BEGIN
+	SELECT nombre, presupuesto(presupuesto,gastos) AS valor_presupuesto -- FUNCION presupuesto PUNTO 11
+	FROM departamento
+	ORDER BY valor_presupuesto ASC;
+END //
+DELIMITER ;
+
+CALL nombreD_presupuestoActual();
 
 -- 13.Lista el nombre de todos los departamentos ordenados de forma ascendente.
 DELIMITER //
@@ -486,10 +493,6 @@ DELIMITER ;
 CALL departamento3();
 
 -- 36.Lista los nombres, apellidos y nif de los empleados que trabajan en los departamentos 2, 4 o 5.
-SELECT nombre,apellido1,apellido2,nif
-FROM empleado
-WHERE id_departamento IN(2,4,5);
-
 DELIMITER //
 CREATE PROCEDURE departamento245()
 BEGIN
@@ -500,5 +503,147 @@ END //
 DELIMITER ;
 
 CALL departamento245();
+
+-- ########### CONSULTA MULTITABLA INTERNA ################
+-- 1.Devuelve un listado con los empleados y los datos de los departamentos donde trabaja cada uno.
+DELIMITER //
+CREATE PROCEDURE empleados_departamento()
+BEGIN 
+	SELECT e.id,e.nombre,e.apellido1,e.apellido2,d.id AS id_departamento,d.nombre,d.presupuesto,d.gastos
+	FROM empleado e
+	INNER JOIN departamento d ON d.id = e.id_departamento;
+END //
+DELIMITER ;
+
+CALL empleados_departamento();
+
+-- 2.Devuelve un listado con los empleados y los datos de los departamentos donde trabaja cada uno. 
+-- Ordena el resultado, en primer lugar por el nombre del departamento (en orden alfabético) 
+-- y en segundo lugar por los apellidos y el nombre de los empleados.
+DELIMITER //
+CREATE PROCEDURE empleados_departamento()
+BEGIN 
+	SELECT e.id,e.nombre,e.apellido1,e.apellido2,d.id AS id_departamento,d.nombre,d.presupuesto,d.gastos
+	FROM empleado e
+	INNER JOIN departamento d ON d.id = e.id_departamento
+    ORDER BY d.nombre,e.apellido1,e.apellido2,e.nombre ASC;
+END //
+DELIMITER ;
+
+CALL empleados_departamento();
+
+-- 3.Devuelve un listado con el identificador y el nombre del departamento,solamente de aquellos 
+-- departamentos que tienen empleados.
+DELIMITER //
+CREATE PROCEDURE departamentoConEmpleados()
+BEGIN 
+	SELECT DISTINCT d.id,d.nombre
+	FROM departamento d
+	INNER JOIN empleado e ON e.id_departamento = d.id
+	WHERE e.id_departamento IS NOT NULL;
+END //
+DELIMITER ;
+
+CALL departamentoConEmpleados();
+
+-- 4.Devuelve un listado con el identificador, el nombre del departamento y el valor del presupuesto
+-- actual del que dispone, solamente de aquellos departamentos que tienen empleados. El valor del 
+-- presupuesto actual lo puede calcular restando al valor del presupuesto inicial (columna presupuesto) 
+-- el valor de los gastos que ha generado (columna gastos).
+DELIMITER //
+CREATE PROCEDURE presupuesto_DepartamentoConEmpleados()
+BEGIN 
+	SELECT DISTINCT d.id, d.nombre, presupuesto(presupuesto,gastos) AS Presupuesto_Actual -- FUNCION presupuesto PUNTO 11
+	FROM departamento d
+	INNER JOIN empleado e ON e.id_departamento = d.id
+	WHERE e.id_departamento IS NOT NULL;
+END //
+DELIMITER ;
+
+CALL presupuesto_DepartamentoConEmpleados();
+
+-- 5.Devuelve el nombre del departamento donde trabaja el empleado que tiene el nif 38382980M.
+DELIMITER //
+CREATE PROCEDURE departamentoConNif()
+BEGIN 
+	SELECT d.nombre
+	FROM departamento d
+	INNER JOIN empleado e ON e.id_departamento = d.id
+	WHERE e.nif = '38382980M';
+END //
+DELIMITER ;
+
+CALL departamentoConNif();
+
+-- 6.Devuelve el nombre del departamento donde trabaja el empleado Pepe Ruiz Santana.
+DELIMITER //
+CREATE PROCEDURE departamentoPepe()
+BEGIN 
+	SELECT d.nombre
+	FROM departamento d
+	INNER JOIN empleado e ON e.id_departamento = d.id
+	WHERE CONCAT(e.nombre,' ',e.apellido1,' ',e.apellido2) LIKE 'Pepe Ruiz Santana';
+END //
+DELIMITER ;
+
+CALL departamentoPepe();
+
+-- 7.Devuelve un listado con los datos de los empleados que trabajan en el departamento de I+D. 
+-- Ordena el resultado alfabéticamente.
+DELIMITER //
+CREATE PROCEDURE departamentoID()
+BEGIN 
+	SELECT e.id,e.nif,e.nombre,e.apellido1,e.apellido2
+	FROM departamento d
+	INNER JOIN empleado e ON e.id_departamento = d.id
+	WHERE d.nombre = 'I+D'
+	ORDER BY e.nombre,e.apellido1,e.apellido2 ASC;
+END //
+DELIMITER ;
+
+CALL departamentoID();
+
+-- 8.Devuelve un listado con los datos de los empleados que trabajan en el departamento de Sistemas,
+-- Contabilidad o I+D. Ordena el resultado alfabéticamente.
+DELIMITER //
+CREATE PROCEDURE departamento_Sis_Cont_ID()
+BEGIN 
+	SELECT e.id,e.nif,e.nombre,e.apellido1,e.apellido2,d.nombre
+	FROM departamento d
+	INNER JOIN empleado e ON e.id_departamento = d.id
+	WHERE d.nombre IN('Sistemas','Contabilidad','I+D')
+	ORDER BY e.nombre,e.apellido1,e.apellido2 ASC;
+END //
+DELIMITER ;
+
+CALL departamento_Sis_Cont_ID();
+
+-- 9.Devuelve una lista con el nombre de los empleados que tienen los departamentos 
+-- que no tienen un presupuesto entre 100000 y 200000 euros.
+DELIMITER //
+CREATE PROCEDURE empleados_presupuesto()
+BEGIN 
+	SELECT e.nombre,d.nombre,d.presupuesto
+	FROM empleado e 
+	INNER JOIN departamento d ON d.id = e.id_departamento
+	WHERE d.presupuesto NOT BETWEEN 100000 AND 200000;
+END //
+DELIMITER ;
+
+CALL empleados_presupuesto();
+
+-- 10.Devuelve un listado con el nombre de los departamentos donde existe algún empleado cuyo segundo 
+-- apellido sea NULL. Tenga en cuenta que no debe mostrar nombres de departamentos que estén repetidos.
+DELIMITER //
+CREATE PROCEDURE departamento_apellido2Null()
+BEGIN 
+	SELECT DISTINCT d.nombre
+	FROM departamento d
+	INNER JOIN empleado e ON d.id = e.id_departamento
+	WHERE e.apellido2 IS NULL;
+END //
+DELIMITER ;
+
+CALL departamento_apellido2Null();
 
 -- REALIZADO POR PAULA MUÑOZ/ID.1.095.953.057
