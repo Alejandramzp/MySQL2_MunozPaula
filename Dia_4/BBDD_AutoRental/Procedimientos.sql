f7a67bf274dbb6bad921ca3523f537a426c3a965
Paula Muñoz <156429313+Alejandramzp@users.noreply.github.com>
2024-06-26 18:57:34 -0500

    feat: :sparkles: Procedimientos

---
+++ Dia_4/BBDD_AutoRental/Procedimientos.sql
@@ -0,0 +1,51 @@
+-- ##########################
+-- ##### PROCEDIMIENTOS #####
+-- ##########################
+
+USE auto_rental;
+
+-- Consulta de disponibilidad de vehículos para alquiler por tipo de vehículo, rango de
+-- precios de alquiler y fechas de disponibilidad.
+
+DELIMITER //
+CREATE PROCEDURE disponibilidad_vehiculos()
+BEGIN
+	SELECT DISTINCT v.id,v.tipo,v.placa,v.referencia,v.modelo,v.color,a.alquiler_semana,a.alquiler_dia
+	FROM vehiculo v
+	INNER JOIN alquiler a ON a.id_vehiculo = v.id
+	WHERE (a.fecha_llegada < a.fecha_esperada_llegada) AND (v.tipo = 'Deportivo') 
+	AND (a.alquiler_semana BETWEEN 150 AND 200); 
+END //
+DELIMITER ;
+
+CALL disponibilidad_vehiculos;
+
+-- Alquiler de vehículos.
+
+DELIMITER //
+CREATE PROCEDURE alquiler_vehiculos()
+BEGIN
+	SELECT * FROM alquiler;
+END //
+DELIMITER ;
+
+CALL alquiler_vehiculos;
+
+-- Consulta de historial de alquileres.
+
+DELIMITER //
+CREATE PROCEDURE historial_alquileres()
+BEGIN
+	SELECT v.tipo,v.placa,v.referencia,v.modelo,v.motor,v.color,
+    CONCAT(c.nombres,' ',c.apellidos,' CC.',c.cedula) AS Cliente,
+    CONCAT(e.nombres,' ',e.apellidos,' CC.',e.cedula) AS Empleado, s.ciudad AS Sucursal,
+    a.fecha_salida,a.fecha_llegada,a.fecha_esperada_llegada
+    FROM alquiler a
+    INNER JOIN vehiculo v ON v.id = a.id_vehiculo
+    INNER JOIN cliente c ON c.id = a.id_cliente
+    INNER JOIN empleado e ON e.id = a.id_empleado
+    INNER JOIN sucursal s ON s.id = e.id_sucursal;
+END //
+DELIMITER ;
+
+CALL historial_alquileres();
