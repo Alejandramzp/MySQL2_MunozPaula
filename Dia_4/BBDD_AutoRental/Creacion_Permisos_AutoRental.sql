954ad8b2f945bd85fa888593ab03a2de3f1d857f
Paula Muñoz <156429313+Alejandramzp@users.noreply.github.com>
2024-06-26 18:56:51 -0500

    feat: :sparkles: Creacion de usuarios

--- Dia_4/BBDD_AutoRental/Creacion_Permisos_AutoRental.sql
+++ Dia_4/BBDD_AutoRental/Creacion_Permisos_AutoRental.sql
@@ -1,10 +1,25 @@
--- ####################
--- ##### USUARIOS #####
--- ####################
+-- ################################
+-- ##### CREACION DE USUARIOS #####
+-- ################################
 
 USE auto_rental;
 
--- Creacion de usuarios de AutoRent
+-- +++++++ Creacion de usuarios de AutoRent +++++++
+
+-- ++++++++ Administrador ++++++++ 
 CREATE USER 'administrador'@'%' IDENTIFIED BY 'administrador123';
-CREATE USER 'ampleado'@'%' IDENTIFIED BY 'empleado456';
+GRANT SELECT,INSERT,UPDATE,DELETE ON auto_rental.* TO 'administrador'@'%';
+
+-- ++++++++ Empleado ++++++++ 
+CREATE USER 'empleado'@'%' IDENTIFIED BY 'empleado456';
+GRANT SELECT,INSERT,UPDATE ON auto_rental.vehiculo to 'empleado'@'%';
+GRANT SELECT,INSERT,UPDATE ON auto_rental.alquiler to 'empleado'@'%';
+
+-- ++++++++ CLiente ++++++++ 
 CREATE USER 'cliente'@'%' IDENTIFIED BY 'cliente789';
+GRANT SELECT ON auto_rental.vehiculo to 'cliente'@'%';
+GRANT SELECT ON auto_rental.alquiler to 'cliente'@'%';
+
+SELECT * FROM mysql.user;
+
+FLUSH PRIVILEGES;
