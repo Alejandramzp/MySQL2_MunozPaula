d236ae345d223383a245fdd44f4e01b23a9e4aaa
Paula Muñoz <156429313+Alejandramzp@users.noreply.github.com>
2024-06-26 22:30:08 -0500

    feat: :sparkles: Procedimientos y Funciones

--- Dia_4/BBDD_AutoRental/Procedimientos.sql
+++ Dia_4/BBDD_AutoRental/Procedimientos.sql
@@ -49,3 +49,36 @@ END //
 DELIMITER ;
 
 CALL historial_alquileres();
+
+-- Si un cliente entrega el vehículo pasada la fecha de entrega contratada, se cobrarán los días
+-- adicionales con un incremento del 8%.
+
+DELIMITER //
+CREATE FUNCTION incremento_retraso(id_vehiculo INT)
+RETURNS DECIMAL(10,2) DETERMINISTIC
+BEGIN
+    DECLARE Valor_incremento DECIMAL(10,2) DEFAULT 0.00;
+    DECLARE dias INT;
+    DECLARE fecha_llegada2 DATE;
+    DECLARE fecha_esperada_llegada2 DATE;
+    DECLARE alquiler_dia2 DECIMAL(10,2);
+    
+    SELECT fecha_llegada, fecha_esperada_llegada, alquiler_dia 
+    INTO fecha_llegada2, fecha_esperada_llegada2, alquiler_dia2
+    FROM alquiler
+    WHERE alquiler.id_vehiculo = id_vehiculo
+    LIMIT 1;
+    
+    IF fecha_llegada2 > fecha_esperada_llegada2 THEN
+        SET dias = DATEDIFF(fecha_llegada2, fecha_esperada_llegada2);
+        SET Valor_incremento = (alquiler_dia2 * dias * 1.08);
+    END IF;
+    
+    RETURN Valor_incremento;
+END //
+DELIMITER ;
+
+-- TEST
+SELECT incremento_retraso(20);
+
+DROP FUNCTION incremento_retraso;
